cdbf808dfc92238c3e6cd3825be1a5f9046cf08f
sKyleWang <36742284+sKyleWang@users.noreply.github.com>
2018-03-13 23:53:29 -0400

    Add files via upload

---
+++ quiz3/src/main/java/pkgCore/Deck.java
@@ -0,0 +1,68 @@
+package pkgCore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+public class Deck {
+
+	//	Add 'cards' attribute that is an ArrayList of Card
+	private ArrayList<Card> DeckCards = new ArrayList<Card>();
+	//	Add a constructor that passes in the number of decks, and then populates
+	//			ArrayList<Card> with cards (depending on number of decks).
+
+	public Deck() 
+	{
+	
+		for (eSuit eSuit: eSuit.values())
+		{
+			for(eRank eRank: eRank.values())
+			{
+				DeckCards.add(new Card(eRank, eSuit));
+				
+			}
+		}
+		Collections.shuffle(DeckCards);
+	}
+
+	
+
+	//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
+	//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
+	
+	//			Deck(2) will create an array of 104 cards.
+	
+
+	public Card draw()
+	{
+		return DeckCards.remove(0);
+	}
+	
+	public int getRemaining(Object eNum)
+	{
+		int count = 0;
+		if (eNum instanceof eRank) 
+		{
+			for (Card c : cards) 
+			{
+				if(c.geteRank() == eNum)
+				{
+					count++;
+				}
+			}
+		}
+		else
+		{
+			for(Card c : cards)
+			{
+				if(c.getSuit() == eNum)
+				{ 
+					conut++;
+				}
+			}
+		}
+			return count;	
+	}
+	
+}
